8a336c4d21c79d2124f8d938b388c387eee7d31f
Ashish Bhardwaj <erashish41@gmail.com>
2025-08-20 17:56:01 +0530

    urls added for DRF

---
+++ hotel_management/hotel/api_modules/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from hotel.api_modules.views import (
+    HotelViewSet, RoomViewSet, ReviewViewSet,
+    FacilityViewSet, DestinationViewSet
+)
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'hotels', HotelViewSet)
+router.register(r'rooms', RoomViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'facilities', FacilityViewSet)
+router.register(r'destinations', DestinationViewSet)
+
+urlpatterns = [
+    path('v2/', include(router.urls))
+]
