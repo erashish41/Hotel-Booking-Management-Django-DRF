ca5630b837a2e76046c2aa9bc95cef2a70fd9ff6
Ashish Bhardwaj <erashish41@gmail.com>
2025-12-21 21:02:23 +0530

    postgre sql db added

--- hotel_management/hotel_management/settings.py
+++ hotel_management/hotel_management/settings.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = 'hotel_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_django',
+        'USER': 'django_user',
+        'PASSWORD': 'django_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
